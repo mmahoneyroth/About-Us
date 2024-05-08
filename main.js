4f5e9a1ad86104bc573d0f39ab3a15d89ce8e36e
Megan Mahoney <mmahoney@rothstaffing.com>
2024-05-08 12:46:04 -0700

    Adding electron/electron forge electron make

---
+++ main.js
@@ -0,0 +1,35 @@
+﻿const { app, BrowserWindow, ipcMain  } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        autoHideMenuBar: true,
+        icon: 'images/icon.ico',
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+
+    win.maximize()
+}
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
+    
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
